febfe9ffc29dbef80b98675c5999b2289a62e2b0
SANDEEP BADETI <sandeepbadeti7730@gmail.com>
2023-01-27 11:56:23 +0530

    error resolved - strict mode enabled

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  // <StrictMode>
+  <StrictMode>
   <AuthContextProvider>
     <App />
   </AuthContextProvider>
-  // </StrictMode>
+   </StrictMode>
 );
